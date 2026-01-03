78a462a353a8daedbdcd7e16471fe36eedda87d8
michelangelo saveriano <michelangelo.saveriano.1998@gmail.com>
2026-01-03 22:07:55 +0100

    Add support for arrow, workflow, return, numbers and strings in the tokenizer

--- kira/klanguage/ktokenizer.py
+++ kira/klanguage/ktokenizer.py
@@ -11,6 +11,7 @@ class KTokenType(enum.Enum):
     DOT = "."
     COLON = ":"
     SEMICOLON = ";"
+    ARROW = "->"
 
     END_LINE = "\n"
     WHITESPACE = " \t"
@@ -35,7 +36,11 @@ class KTokenType(enum.Enum):
     AND = "and"
     OR = "or"
 
-    QUOTES = "'\""
+    WORKFLOW = "workflow"
+    RETURN = "return"
+
+    STRING = "'\""
+    NUMBER = "0123456789.eE-+"
 
 
 class KToken(NamedTuple):
@@ -58,7 +63,27 @@ def ktokenize(expression: str):
         elif char in KTokenType.COMMA.value:
             tokens.append(KToken(char, KTokenType.COMMA))
         elif char in KTokenType.DOT.value:
-            tokens.append(KToken(char, KTokenType.DOT))
+            # Check if it's a number starting with dot (e.g. .89)
+            if stop_idx + 1 < len(expression) and expression[stop_idx + 1].isdigit():
+                start_idx = stop_idx
+                stop_idx += 1
+                while stop_idx < len(expression) and expression[stop_idx].isdigit():
+                    stop_idx += 1
+                # Support scientific notation
+                if stop_idx < len(expression) and expression[stop_idx] in "eE":
+                    e_pos = stop_idx
+                    stop_idx += 1
+                    if stop_idx < len(expression) and expression[stop_idx] in "+-":
+                        stop_idx += 1
+                    if stop_idx < len(expression) and expression[stop_idx].isdigit():
+                        while stop_idx < len(expression) and expression[stop_idx].isdigit():
+                            stop_idx += 1
+                    else:
+                        stop_idx = e_pos # Backtrack
+                tokens.append(KToken(expression[start_idx:stop_idx], KTokenType.NUMBER))
+                stop_idx -= 1
+            else:
+                tokens.append(KToken(char, KTokenType.DOT))
         elif char in KTokenType.COLON.value:
             tokens.append(KToken(char, KTokenType.COLON))
         elif char in KTokenType.SEMICOLON.value:
@@ -102,21 +127,49 @@ def ktokenize(expression: str):
         elif char in KTokenType.PLUS.value:
             tokens.append(KToken(char, KTokenType.PLUS))
         elif char in KTokenType.MINUS.value:
-            tokens.append(KToken(char, KTokenType.MINUS))
+            if stop_idx + 1 < len(expression) and expression[stop_idx + 1] == ">":
+                tokens.append(KToken(char + expression[stop_idx + 1], KTokenType.ARROW))
+                stop_idx += 1
+            else:
+                tokens.append(KToken(char, KTokenType.MINUS))
         elif char in KTokenType.MULTIPLY.value:
             tokens.append(KToken(char, KTokenType.MULTIPLY))
         elif char in KTokenType.DIVIDE.value:
             tokens.append(KToken(char, KTokenType.DIVIDE))
         elif char in KTokenType.EXPONENT.value:
             tokens.append(KToken(char, KTokenType.EXPONENT))
-        elif char in KTokenType.QUOTES.value:
-            start_idx = stop_idx + 1
-            while stop_idx < len(expression) and expression[stop_idx] != char:
+        elif char in KTokenType.STRING.value:
+            quote_char = char
+            stop_idx += 1
+            start_idx = stop_idx
+            while stop_idx < len(expression) and expression[stop_idx] != quote_char:
                 stop_idx += 1
-            tokens.append(KToken(expression[start_idx:stop_idx], KTokenType.QUOTES))
+            if stop_idx >= len(expression):
+                raise SyntaxError(f"Unterminated string literal at {start_idx-1}")
+            tokens.append(KToken(expression[start_idx:stop_idx], KTokenType.STRING))
+        elif char.isdigit():
+            start_idx = stop_idx
+            while stop_idx < len(expression) and expression[stop_idx].isdigit():
+                stop_idx += 1
+            if stop_idx < len(expression) and expression[stop_idx] == ".":
+                stop_idx += 1
+                while stop_idx < len(expression) and expression[stop_idx].isdigit():
+                    stop_idx += 1
+            if stop_idx < len(expression) and expression[stop_idx] in "eE":
+                e_pos = stop_idx
+                stop_idx += 1
+                if stop_idx < len(expression) and expression[stop_idx] in "+-":
+                    stop_idx += 1
+                if stop_idx < len(expression) and expression[stop_idx].isdigit():
+                    while stop_idx < len(expression) and expression[stop_idx].isdigit():
+                        stop_idx += 1
+                else:
+                    stop_idx = e_pos # Backtrack
+            tokens.append(KToken(expression[start_idx:stop_idx], KTokenType.NUMBER))
+            stop_idx -= 1
         else:
             start_idx = stop_idx
-            while stop_idx < len(expression) and (expression[stop_idx].isalnum() or expression[stop_idx] in "_."):
+            while stop_idx < len(expression) and (expression[stop_idx].isalnum() or expression[stop_idx] == "_"):
                 stop_idx += 1
 
             word = expression[start_idx:stop_idx]
@@ -125,6 +178,10 @@ def ktokenize(expression: str):
                 tokens.append(KToken(word, KTokenType.AND))
             elif word == "or":
                 tokens.append(KToken(word, KTokenType.OR))
+            elif word == "workflow":
+                tokens.append(KToken(word, KTokenType.WORKFLOW))
+            elif word == "return":
+                tokens.append(KToken(word, KTokenType.RETURN))
             else:
                 tokens.append(KToken(word, KTokenType.SYMBOL))
 
